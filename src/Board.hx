d240f8b221927587f448a42ef9db8f56ba862668
Louis Pearson <desttinghimgame@gmail.com>
2017-03-24 13:42:24 -0500

    Add basic board interaction
    I don't like it. Will change to using mint + buttons

--- src/Board.hx
+++ src/Board.hx
@@ -49,4 +49,66 @@ class Board extends State {
 
     }
 
+    static var box1 = {
+        a: {x: 0.0, y: 0.0},
+        b: {x: 1.0/3, y: 1.0/3},
+    };
+
+    static var box2 = {
+        a: {x: 1.0/3, y: 0.0},
+        b: {x: 2.0/3, y: 1.0/3},
+    };
+
+    static var box3 = {
+        a: {x: 2.0/3, y: 0.0},
+        b: {x: 3.0, y: 1.0/3},
+    };
+
+    static var box4 = {
+        a: {x: 0.0, y: 1.0/3},
+        b: {x: 1.0/3, y: 2.0/3},
+    };
+
+    static var box5 = {
+        a: {x: 1.0/3, y: 1.0/3},
+        b: {x: 2.0/3, y: 2.0/3},
+    };
+
+    static var box6 = {
+        a: {x: 2.0/3, y: 1.0/3},
+        b: {x: 3.0, y: 2.0/3},
+    };
+
+    static var box7 = {
+        a: {x: 0.0, y: 3.0},
+        b: {x: 1.0/3, y: 2.0/3},
+    };
+
+    static var box8 = {
+        a: {x: 1.0/3, y: 3.0},
+        b: {x: 2.0/3, y: 2.0/3}
+    };
+
+    static var box9 = {
+        a: {x: 2.0/3, y: 3.0},
+        b: {x: 3.0, y: 2.0/3}
+    };
+
+    override function onmouseup(e:luxe.MouseEvent) {
+
+        function checkAABB(box:{a:{x:Float,y:Float},b:{x:Float,y:Float}}, pos) {
+            if (pos.x > box.a.x && pos.x < box.b.x && pos.y > box.a.y && pos.y < box.b.y) {
+                return true;
+            }
+            return false;
+        }
+
+        if (grid.point_inside_AABB(e.pos)) {
+            var pos = e.pos.subtract(grid.pos).divideScalar(480);
+
+            if (checkAABB(box1, pos)) {trace('square 1');}
+        }
+
+    }
+
 }
