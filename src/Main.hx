33efd977a40a135c21136e74188b26c8848b322b
Louis Pearson <desttinghimgame@gmail.com>
2017-03-24 11:35:29 -0500

    Init with luxe Empty project

---
+++ src/Main.hx
@@ -0,0 +1,34 @@
+
+import luxe.GameConfig;
+import luxe.Input;
+
+class Main extends luxe.Game {
+
+    override function config(config:GameConfig) {
+
+        config.window.title = 'luxe game';
+        config.window.width = 960;
+        config.window.height = 640;
+        config.window.fullscreen = false;
+
+        return config;
+
+    } //config
+
+    override function ready() {
+
+    } //ready
+
+    override function onkeyup(event:KeyEvent) {
+
+        if(event.keycode == Key.escape) {
+            Luxe.shutdown();
+        }
+
+    } //onkeyup
+
+    override function update(delta:Float) {
+
+    } //update
+
+} //Main
